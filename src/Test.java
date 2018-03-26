aca51ce3dca8f4c226fe84ef3dfd168fc1f9bead
Brett Koppelman <brettkoppelman01@gmail.com>
2018-03-26 08:52:38 -0500

    Fixed Even Number

--- src/Test.java
+++ src/Test.java
@@ -4,50 +4,46 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Test {
 
-	   public static void main(String[] args) throws IOException {
-		   
-Scanner input2 = new Scanner(System.in);
-System.out.println("Enter File Name: ");
-String x = input2.nextLine();
-File doc = new File(x);
-	        Scanner input = new Scanner(doc);
-	        Scanner input4 = new Scanner(doc);
-	        File y = new File("bob");
-	        FileOutputStream outputStream = new FileOutputStream(y);
-	        PrintWriter output = new PrintWriter(outputStream);
+	public static void main(String[] args) throws IOException {
 
-	        int count = 0;	       
-	        while (input.hasNext()) {
-	           count+=input.nextInt();	
-	        }
-	        while(input4.hasNext()) 
-	        	if(input4.nextInt()%2==0) {
-	        		System.out.println(input4.nextInt());
-	        	}
-	        
-	        output.close();
-	        System.out.println("Sum: " + count);
-	        
-	        /*
-	         * Scanner input2 = new Scanner(System.in);
+		Scanner input2 = new Scanner(System.in);
 		System.out.println("Enter File Name: ");
 		String x = input2.nextLine();
 		File doc = new File(x);
 		Scanner input = new Scanner(doc);
-		Scanner input3 = new Scanner(doc);
+		Scanner input4 = new Scanner(doc);
 		File y = new File("bob");
 		FileOutputStream outputStream = new FileOutputStream(y);
 		PrintWriter output = new PrintWriter(outputStream);
+
 		int count = 0;
-		while (input.hasNext()) {
-			count += input.nextInt();
+
+		while (input4.hasNext()) {
+			int num = input4.nextInt();
+			count += num;
+			if (num % 2 == 0) {
+				System.out.println(num);
+
+			}
 		}
-		while(input3.hasNext()) System.out.println(input3.nextLine());
+
 		output.close();
+
 		System.out.println("Sum: " + count);
-	         */
-	    }
+
+		/*
+		 * Scanner input2 = new Scanner(System.in);
+		 * System.out.println("Enter File Name: "); String x = input2.nextLine(); File
+		 * doc = new File(x); Scanner input = new Scanner(doc); Scanner input3 = new
+		 * Scanner(doc); File y = new File("bob"); FileOutputStream outputStream = new
+		 * FileOutputStream(y); PrintWriter output = new PrintWriter(outputStream); int
+		 * count = 0; while (input.hasNext()) { count += input.nextInt(); }
+		 * while(input3.hasNext()) System.out.println(input3.nextLine());
+		 * output.close(); System.out.println("Sum: " + count);
+		 */
 	}
+}
