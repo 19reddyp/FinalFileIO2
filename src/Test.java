eb412c30526752cfdfc8bb242fb62326686aa696
Brett Koppelman <brettkoppelman01@gmail.com>
2018-03-25 21:20:15 -0500

    B

--- src/Test.java
+++ src/Test.java
@@ -7,22 +7,30 @@ import java.util.Scanner;
 
 public class Test {
 
-	   public static void main(String[] args) throws IOException {
-		   
-Scanner input2 = new Scanner(System.in);
-System.out.println("Enter File Name: ");
-String x = input2.nextLine();
-File doc = new File(x);
-	        Scanner input = new Scanner(doc);
-	        File y = new File("bob");
-	        FileOutputStream outputStream = new FileOutputStream(y);
-	        PrintWriter output = new PrintWriter(outputStream);
+	public static void main(String[] args) throws IOException {
 
-	        int count = 0;
-	        while (input.hasNext()) {
-	           count+=input.nextInt();	     
-	        }
-	        output.close();
-	        System.out.println("Sum: " + count);
-	    }
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Enter File Name: ");
+		String x = input2.nextLine();
+		File doc = new File(x);
+		Scanner input = new Scanner(doc);
+		File y = new File("bob");
+		FileOutputStream outputStream = new FileOutputStream(y);
+		PrintWriter output = new PrintWriter(outputStream);
+
+		int count = 0;
+		int evenCount = 0;
+		while (input.hasNext()) {
+			if (input.nextInt() % 2 == 0) {
+				evenCount++;
+			}
+		}
+		System.out.println(evenCount);
+		/*
+		 * while (input.hasNext()) { count+=input.nextInt(); } output.close();
+		 * System.out.println("Sum: " + count); while(input.hasNext()) {
+		 * 
+		 * }
+		 */
 	}
+}
