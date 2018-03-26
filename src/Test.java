2563caac2f5a3a20f5e48361000b2b052a9ff8e4
Brett Koppelman <brettkoppelman01@gmail.com>
2018-03-25 21:34:35 -0500

    Even Number

--- src/Test.java
+++ src/Test.java
@@ -14,15 +14,40 @@ System.out.println("Enter File Name: ");
 String x = input2.nextLine();
 File doc = new File(x);
 	        Scanner input = new Scanner(doc);
+	        Scanner input4 = new Scanner(doc);
 	        File y = new File("bob");
 	        FileOutputStream outputStream = new FileOutputStream(y);
 	        PrintWriter output = new PrintWriter(outputStream);
 
-	        int count = 0;
+	        int count = 0;	       
 	        while (input.hasNext()) {
-	           count+=input.nextInt();	     
+	           count+=input.nextInt();	
 	        }
+	        while(input4.hasNext()) 
+	        	if(input4.nextInt()%2==0) {
+	        		System.out.println(input4.nextLine());
+	        	}
+	        
 	        output.close();
 	        System.out.println("Sum: " + count);
+	        
+	        /*
+	         * Scanner input2 = new Scanner(System.in);
+		System.out.println("Enter File Name: ");
+		String x = input2.nextLine();
+		File doc = new File(x);
+		Scanner input = new Scanner(doc);
+		Scanner input3 = new Scanner(doc);
+		File y = new File("bob");
+		FileOutputStream outputStream = new FileOutputStream(y);
+		PrintWriter output = new PrintWriter(outputStream);
+		int count = 0;
+		while (input.hasNext()) {
+			count += input.nextInt();
+		}
+		while(input3.hasNext()) System.out.println(input3.nextLine());
+		output.close();
+		System.out.println("Sum: " + count);
+	         */
 	    }
 	}
